8ddfe9b0d8f967af99131f05728c344a1bb75471
AdanMerino-AT <brayan.merino@agilethought.com>
2020-10-27 18:49:53 -0600

    Build CreateUsers feature and stepdefinitions config

---
+++ src/test/java/com/stepdefinitions/CreateUsers.java
@@ -0,0 +1,71 @@
+package com.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class CreateUsers{
+	
+
+	@Given("I am in the Users Administration page")
+	public void i_am_in_the_Users_Administration_page() throws Throwable{
+		
+	}
+
+	@When("I do click in the AddNewUser button")
+	public void i_do_click_in_the_AddNewUser_button() throws Throwable{
+		
+	}
+
+	@Then("The AddNewUser form should be displayed")
+	public void the_AddNewUser_page_should_be_displayed() throws Throwable{
+		
+	}
+
+	@Then("I put a valid name")
+	public void i_put_a_valid_name() throws Throwable{
+		
+	}
+
+	@Then("I put a valid firstname")
+	public void i_put_a_valid_firstname() throws Throwable{
+		
+	}
+
+	@Then("I put a valid lastname")
+	public void i_put_a_valid_lastname() throws Throwable{
+		
+	}
+
+	@Then("I put a valid email")
+	public void i_put_a_valid_email() throws Throwable{
+		
+	}
+
+	@Then("I put a valid password")
+	public void i_put_a_valid_password() throws Throwable{
+		
+	}
+
+	@Then("I select a status")
+	public void i_select_a_status() throws Throwable{
+		
+	}
+
+	@Then("I do click in the Save button")
+	public void i_do_click_in_the_Save_button() throws Throwable{
+		
+	}
+
+	@Then("I should see an alert of successful user creation")
+	public void i_should_see_an_alert_of_successful_user_creation() throws Throwable{
+		
+	}
+
+	@Then("The user should be found by the search function of the page")
+	public void the_user_should_be_found_by_the_search_function_of_the_page() throws Throwable{
+	
+	}
+
+}
+
---
+++ src/test/java/com/stepdefinitions/RunCakeTest.java
@@ -0,0 +1,14 @@
+package com.stepdefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"},
+        features = "src/test/resources/",
+        glue = "com.stepdefinitions",
+        monochrome = false)
+public class RunCakeTest {
+
+}
